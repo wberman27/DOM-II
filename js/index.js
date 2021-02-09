032d312284bc551a437c90132ff5d7545389ec70
Will B <wberman27@gmail.com>
2021-02-09 14:25:07 -0500

    update 1, added mouseover and mouseout on images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const images = document.getElementsByTagName("img");
+Array.from(images).forEach(img => {
+    img.addEventListener("mouseover", (e) => {
+        img.style.borderStyle = "solid"
+        img.style.borderWidth = "5px"
+    })
+    img.addEventListener("mouseout", (e) => {
+        img.style.borderStyle = "none"
+        img.style.borderWidth = "0px"
+    })
+})
+
